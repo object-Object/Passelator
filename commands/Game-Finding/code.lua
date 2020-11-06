af9d029d4b9125f3d5e6e65a68a132ac58b84db2
[object Object] <37044997+object-Object@users.noreply.github.com>
2020-11-06 05:38:06 +0000

    authorization stuff

--- commands/Game-Finding/code.lua
+++ commands/Game-Finding/code.lua
@@ -1,10 +1,11 @@
 local commandHandler = require("commandHandler")
 local utils = require("miscUtils")
 local groupUtils = require("groupUtils")
+local discordia = require("discordia")
 
 return {
 	name = "code",
-	description = "Displays or sets the game link/code for a specific game.",
+	description = "Displays or sets the game link/code for a specific group.",
 	usage = "<game number> [new game code or link]",
 	visible = true,
 	permissions = {},
@@ -23,26 +24,67 @@ return {
 			return
 		end
 
+		local groupAuthor = message.client:getUser(row.author_id)
+		local newCode = argString:gsub("^"..args[1].."%s+", "")
+
+		local promptMessage
 		if #args==1 then
 			-- display code
-			utils.sendEmbed(message.channel, "Game code/link for Game #"..row.game_num..": "..row.game_code, "00ff00")
-		elseif message.author.id==row.author_id then
-			-- set code
-			local newCode = argString:gsub("^"..args[1].."%s+", "")
-			local stmt2 = conn:prepare("UPDATE games SET game_code = ? WHERE guild_id = ? AND game_num = ?;")
-			stmt2:reset():bind(newCode, row.guild_id, row.game_num):step()
-			stmt2:close()
+			utils.sendEmbed(message.channel, "Game code/link for Game #"..row.game_num..": **"..row.game_code.."**", "00ff00")
+			return
+		elseif message.author~=groupAuthor then
+			-- not the author, ask author to confirm/deny
+			promptMessage = message:reply{
+				content = groupAuthor.mentionString,
+				embed = {
+					description = "Only the creator of a group may set the game code/link.\n"..groupAuthor.mentionString..", react :white_check_mark: to confirm, or :x: to deny, setting the game code/link for Game #"..row.game_num.." to the following code: **"..newCode.."**",
+					color = discordia.Color.fromHex("00ff00").value,
+					footer = {
+						text = "This message will expire in 5 minutes."
+					}
+				}
+			}
+			promptMessage:addReaction("✅")
+			promptMessage:addReaction("❌")
+			local success, reaction, userId = message.client:waitFor("reactionAdd", 300000, function(r, u) -- 300000 ms = 5 min
+				return u==groupAuthor.id and r.message.id==promptMessage.id and (r.emojiHash=="✅" or r.emojiHash=="❌")
+			end)
+			if not success then
+				promptMessage:clearReactions()
+				promptMessage:setEmbed{
+					description = "Only the creator of a group may set the game code/link.\nMessage expired.",
+					color = discordia.Color.fromHex("ff0000").value
+				}
+				return
+			elseif reaction.emojiHash=="❌" then
+				promptMessage:clearReactions()
+				promptMessage:setEmbed{
+					description = "Only the creator of a group may set the game code/link.\nGroup creator denied setting this code.",
+					color = discordia.Color.fromHex("ff0000").value
+				}
+				return
+			end
+		end
 
-			local role = message.guild:getRole(row.role_id)
-			local voiceChannel = message.guild:getChannel(row.voice_channel_id)
-			local gamesChannel = message.guild:getChannel(guildSettings.games_channel_id)
-			local gameMessage = gamesChannel:getMessage(row.message_id)
+		local stmt2 = conn:prepare("UPDATE games SET game_code = ? WHERE guild_id = ? AND game_num = ?;")
+		stmt2:reset():bind(newCode, row.guild_id, row.game_num):step()
+		stmt2:close()
 
-			gameMessage:setEmbed(groupUtils.getGroupEmbed(message.author, row.game_num, row.name, role, voiceChannel, row.voice_channel_invite, newCode))
+		local role = message.guild:getRole(row.role_id)
+		local voiceChannel = message.guild:getChannel(row.voice_channel_id)
+		local gamesChannel = message.guild:getChannel(guildSettings.games_channel_id)
+		local gameMessage = gamesChannel:getMessage(row.message_id)
 
-			utils.sendEmbed(message.channel, "The game code/link for Game #"..row.game_num.." is now: "..newCode, "00ff00")
+		gameMessage:setEmbed(groupUtils.getGroupEmbed(message.author, row.game_num, row.name, role, voiceChannel, row.voice_channel_invite, newCode))
+
+		if promptMessage then
+			promptMessage:clearReactions()
+			promptMessage:setEmbed{
+				description = "The game code/link for Game #"..row.game_num.." is now: **"..newCode.."**",
+				color = discordia.Color.fromHex("00ff00").value
+			}
 		else
-			utils.sendEmbed(message.channel, "Only the creator of the game group may set the game code/link.", "ff0000")
+			utils.sendEmbed(message.channel, "The game code/link for Game #"..row.game_num.." is now: **"..newCode.."**", "00ff00")
 		end
 	end,
 	onEnable = function(self, message, guildSettings)
