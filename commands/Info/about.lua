217c42b990a365a709325bd298ccd87142c0b0a1
[object Object] <37044997+object-Object@users.noreply.github.com>
2020-12-08 01:53:13 +0000

    make prettier

--- commands/Info/about.lua
+++ commands/Info/about.lua
@@ -16,19 +16,20 @@ return {
 		message.channel:send{
 			embed = {
 				title = "About",
-				description = "Passelator is a bot designed to help with game-finding, especially for Among Us. It is written (in Lua), hosted, and maintained by [object Object]#0001.",
+				description = "Passelator is a bot that helps schedule games by allowing users to create custom, self-assignable roles, and private voice channels for those roles. It is written (in Lua), hosted, and maintained by [object Object]#0001.",
 				color = discordia.Color.fromHex("00ff00").value,
 				fields = {
-					{name = "Getting started", value = [[**1.** Create a new group using `]]..guildSettings.prefix..[[new <group name>`.
+					{name = "Getting Started", value = [[**1.** Create a new group using `]]..guildSettings.prefix..[[new <group name>`.
 **2.** Go to the groups channel and get the role by clicking :white_check_mark:.
 **3.** Edit the group's settings using `]]..guildSettings.prefix..[[code`, `]]..guildSettings.prefix..[[datetime`, etc. See `]]..guildSettings.prefix..[[help` for more commands.
 **4.** Invite your friends to join your new group.
 **5.** When your game is ready to start, ping the group's custom role, join its voice channel, and play some games!
 **Note:** The server admins need to first set up the bot using `]]..guildSettings.prefix..[[settings`. Specifically, the Group Category and Group Channel settings must be set before the groups feature may be used.]]},
 					{name = "Servers", value = #message.client.guilds},
+					{name = "Invite Link", value = "[Click to invite Passelator to your server!](https://discord.com/api/oauth2/authorize?client_id=772674051076128789&permissions="..tonumber(discordia.storage.requiredPermissions:toHex()).."&scope=bot)"},
 					{name = "GitHub", value = "https://github.com/object-Object/Passelator"},
-					{name = "Invite link", value = "https://discord.com/api/oauth2/authorize?client_id=772674051076128789&permissions="..tonumber(discordia.storage.requiredPermissions:toHex()).."&scope=bot"},
-					{name = "Contact me", value = "Use the command `"..guildSettings.prefix.."contact`."},
+					{name = "Vote for Passelator", value = "[top.gg](https://top.gg/bot/772674051076128789/vote)\n[Discord Bot List](https://discordbotlist.com/bots/passelator/upvote)"},
+					{name = "Contact Me", value = "Found a bug? Have a suggestion? Tell me about it! Use the command `"..guildSettings.prefix.."contact` to send me a message."},
 				},
 				footer = {
 					text = "Version: "..version
